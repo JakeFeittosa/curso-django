fb176ba30dbe31088c9aaf4541c62a073c5f54c1
JakeFeittosa <jackfeitosa1999@gmail.com>
2023-05-12 08:53:33 -0300

    block content

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,10 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Jakeline Feitosa',
     })
+
+
+def recipe(request, id):
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Jakeline Feitosa',
+    })
