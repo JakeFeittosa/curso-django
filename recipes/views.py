aed328b28cdfbf216b2ad3f1a96c88bcffc8dfe1
JakeFeittosa <jackfeitosa1999@gmail.com>
2023-05-25 08:48:38 -0300

    pip install  do faker/ criando o block para o titulo da pagina

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from utils.receita.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Jakeline Feitosa',
+        'recipes': [make_recipe()for _ in range(10)],
     })
 
 
 def recipe(request, id):
 
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Jakeline Feitosa',
+        'recipe': make_recipe(),
     })
